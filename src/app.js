2c3a03e2df18dd4cd9dc03801aa55b1992799309
shefuyuta <shefuyuta@users.noreply.github.com>
2021-03-28 22:01:18 +0900

    added server port

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const server = app.listen(3000, function(){
+    console.log("Node.js is listening to PORT:" + server.address().port);
 app.use(express.static("./"));
 app.set('view engine', 'ejs');
 
