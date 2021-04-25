b8d46b7993cf5f413f7066d55fe8f1038c2ab257
shefuyuta <shefuyuta@users.noreply.github.com>
2021-04-25 21:47:51 +0900

    modifed jquery functions as test

--- js/jquery.js
+++ js/jquery.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-  $("#submenu").on("hover", function(){
+  $(".move").on("hover", function(){
      $(".submenu").slideToggle();
+  });
+});
+
+$(function(){
+  $(".move").mouseover(function(){
+    subHeight = $(this).children(".sub").height();
+    $(".sub").removeClass("active");
+    if( subHeight <= 0 ){
+      $(this).children(".sub").addClass("active");
+    }else{
+      $(this).children(".sub").removeClass("active");
+    }
   });
 });
